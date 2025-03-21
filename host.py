c947796df64b9ca0d9899fb43693a9f4e7415045
oxyum services <29GariglioSe@fortcherry.org>
2025-03-20 21:36:08 -0400

    push

---
+++ host.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home/index.html')
+
+@app.route('/authenticated', methods=['POST'])
+def authenticated():
+    return render_template('dashboard/index.html')
+
+@app.route('/about', methods=['POST'])
+def about():
+    return render_template('about/index.html')
+
+app.run(debug=True)
