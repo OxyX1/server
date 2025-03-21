0fedaafc87889aac07f549afb4c77f4922dd9a3b
oxyum services <29GariglioSe@fortcherry.org>
2025-03-20 22:09:38 -0400

    push

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
-
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-
 app.use(express.static(path.join(__dirname, 'templates')));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'templates', 'home', 'index.html'));
 });
 
-app.use('/authenticated', (req, res) => {
+app.get('/authenticated', (req, res) => {
     res.sendFile(path.join(__dirname, 'templates', 'dashboard', 'index.html'));
 });
 
-app.use('/authentication', (req, res) => {
+app.get('/authentication', (req, res) => {
     res.sendFile(path.join(__dirname, 'templates', 'authentication', 'index.html'));
 });
 
-app.use('/about', (req, res) => {
+app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'templates', 'about', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
